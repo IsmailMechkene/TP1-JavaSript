828df6a064b5bc36c8446bfe71f15a00d282ce57
Ismail Mechkene <imechkene@gmail.com>
2025-09-17 22:03:30 +0200

    fixed

--- Partie1/Exercice 1/ex1.js
+++ Partie1/Exercice 1/ex1.js
@@ -1,15 +1,15 @@
 {
-    var a = 100;   // redefines a outside the block too
-    let b = 200;   // exists only inside this block
-    const c = 300; // exists only inside this block
+    var a = 100; 
+    let b = 200;  
+    const c = 300; 
 
     console.log("Inside block:", a, b, c);
 }
 
-console.log("Outside block:", a); // a = 100 (var ignores block)
-console.log("Outside block:", b); // ❌ Error: b is not defined
-console.log("Outside block:", c); // ❌ Error: c is not defined
+console.log("Outside block:", a); 
+console.log("Outside block:", b); // b is not defined
+console.log("Outside block:", c); // c is not defined
 
 
 const c = 50;
-c = 60; // ❌ TypeError: Assignment to constant variable
+c = 60; // cant assign to constant variable
